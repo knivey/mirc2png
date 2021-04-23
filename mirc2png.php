aaae4d5fd64e512ad89e3edf1afca9c88c7dc06e
Fred Akers <knivey@botops.net>
2021-04-23 15:27:34 -0400

    Few tweaks to char size/pos, support formatting resets

--- mirc2png.php
+++ mirc2png.php
@@ -17,14 +17,15 @@ function loadfile($file) {
     return array_filter(explode("\n", $cont));
 }
 
-function stripcodes(string $text, $color = true): string {
+function stripcodes(string $text, $color = true, $reset = true): string {
     $text = str_replace("\x02", "", $text);
     $text = str_replace("\x1D", "", $text);
     $text = str_replace("\x1F", "", $text);
     $text = str_replace("\x1E", "", $text);
     $text = str_replace("\x11", "", $text);
     $text = str_replace("\x16", "", $text);
-    $text = str_replace("\x0F", "", $text);
+    if($reset)
+        $text = str_replace("\x0F", "", $text);
     if(!$color)
         return $text;
     $colorRegex = "/\x03(\d?\d?)(,\d\d?)?/";
@@ -44,18 +45,30 @@ function convert(string $mircfile, string $pngfile, $size = 12, $font = "./Hack-
     foreach($text as $line) {
         $width = max($width, mb_strlen(stripcodes($line)));
     }
-    //So much for easy monospace, i think this may be the biggest char, this function will reduce size if a char is smaller than its box
+    //So much for easy monospace, this string should give us an idea to calc text block size, the imagettfbox function only gives bare min to draw
     $lol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $rect = imagettfbbox($size, 0, $font, $lol);
     $minX = min(array($rect[0],$rect[2],$rect[4],$rect[6]));
     $maxX = max(array($rect[0],$rect[2],$rect[4],$rect[6]));
     $charW = ($maxX - $minX) / strlen($lol);
-    $rect = imagettfbbox($size, 0, $font, "|");
+    $rect = imagettfbbox($size, 0, $font, $lol);
     $minY = min(array($rect[1],$rect[3],$rect[5],$rect[7]));
     $maxY = max(array($rect[1],$rect[3],$rect[5],$rect[7]));
     $charH = $maxY - $minY;
+    //Some chars hang below baseline, try to see how much for a little nicer render
+    $lol = "$lol;y,_";
+    $rect = imagettfbbox($size, 0, $font, $lol);
+    $minY = min(array($rect[1],$rect[3],$rect[5],$rect[7]));
+    $maxY = max(array($rect[1],$rect[3],$rect[5],$rect[7]));
+    $riseup = ($maxY - $minY) - $charH;
+    //this will be our total height
+    $rect = imagettfbbox($size, 0, $font, '|');
+    $minY = min(array($rect[1],$rect[3],$rect[5],$rect[7]));
+    $maxY = max(array($rect[1],$rect[3],$rect[5],$rect[7]));
+    $charH = ($maxY - $minY) + $riseup;
+
     $height *= $charH;
-    $width *= ($charW);
+    $width *= $charW;
 
     //add a px or two why not
     $im = imagecreatetruecolor($width + 1, $height);
@@ -68,31 +81,41 @@ function convert(string $mircfile, string $pngfile, $size = 12, $font = "./Hack-
         $bg = $colors[1];
         $curX = 0;
         $colorRegex = "/^\x03(\d?\d?)(,\d\d?)?/";
-        $line = stripcodes($line, false);
+        $line = stripcodes($line, false, false);
         for($i=0; $i < strlen($line); $i++) {
             $rem = substr($line, $i);
+
             if(preg_match($colorRegex, $rem, $m)) {
-                $i += strlen($m[0]);
+                $i += strlen($m[0]); // Don't sub 1 because later we locate next color code
                 if(isset($m[1]) && $m[1] != "")
                     $fg = $colors[(int)$m[1]];
+                else
+                    if(!isset($m[2])) {
+                        $fg = $colors[0];
+                        $bg = $colors[1];
+                    }
                 if(isset($m[2])) {
                     $m[2] = substr($m[2], 1);
                     $bg = $colors[(int)$m[2]];
                 }
             }
-            $nextC = strpos($line, "\x03", $i);
-            if($nextC === false) {
-                $chunk = substr($line, $i);
-                $i = strlen($line);
-            } else {
-                $chunk = substr($line, $i, $nextC - $i);
-                $i = $nextC-1;
+            if(!isset($line[$i])) //Line could have ended with a color code
+                continue;
+            //Format reset
+            if($line[$i] == "\x0F") {
+                $fg = $colors[0];
+                $bg = $colors[1];
+                $i++;
             }
+            $nextCode = strcspn($line, "\x03\x0F", $i) + $i;
+            $chunk = substr($line, $i, $nextCode - $i);
+            $i = $nextCode-1;
+
             $x = $curX * $charW;
-            $y = $lineno * $charH;
+            $y = ($lineno * $charH) - $riseup;
             imagefilledrectangle($im, $x, $y, $x + ($charW * mb_strlen($chunk)), $y + $charH, $bg);
             // $y + $charH because lol fonts start at bottom coord..
-            $box = imagettftext($im, $size, 0, $x, $y + $charH, $fg, $font, $chunk);
+            imagettftext($im, $size, 0, $x, $y + $charH - $riseup, $fg, $font, $chunk);
             $curX += mb_strlen($chunk);
         }
         $lineno--;
